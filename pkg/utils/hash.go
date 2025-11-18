814551c85074da9b81573929a1a0a6014115b7c1
simon <simon.c@conso.tg>
2025-11-18 17:35:34 +0800

    test

--- pkg/utils/hash.go
+++ pkg/utils/hash.go
@@ -10,12 +10,8 @@ import (
 
 // 生成 HMAC-SHA256 哈希并将前4个字节转换为 uint32,最后与给定的数字进行求模
 func HMACSHA256FromStringToUint(s string, i *atomic.Int32) uint32 {
+	mod := uint32(0)
 	//几个市值最高的代币分隔开
-	hash := sha256.Sum256([]byte(s))
-	b := hash[0:4]
-	num := binary.BigEndian.Uint32(b[:])
-	poolLength := uint32(i.Load())
-	mod := num % uint32(poolLength)
 	if strings.Contains(s, "btc") {
 		mod = 0
 	} else if strings.Contains(s, "eth") {
@@ -24,6 +20,12 @@ func HMACSHA256FromStringToUint(s string, i *atomic.Int32) uint32 {
 		mod = 2
 	} else if strings.Contains(s, "sol") {
 		mod = 3
+	} else {
+		hash := sha256.Sum256([]byte(s))
+		b := hash[0:4]
+		num := binary.BigEndian.Uint32(b[:])
+		poolLength := uint32(i.Load())
+		mod = num % uint32(poolLength)
 	}
 	return mod
 }
